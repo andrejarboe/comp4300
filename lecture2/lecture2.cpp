473fd32afa2ef987e9c036cb082649e2ef0ecdb5
Andre Jarboe II <andrejarboe@gmail.com>
2024-10-21 22:26:53 -0500

    c++ fopr game dev

---
+++ lecture2/lecture2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char * argv[])
+{
+    int age = 31;
+    std::string first = "Andre";
+    std::string last = "Jarboe";
+    std::string name = first + " " + last;
+
+    std::cout<< name << std::endl;
+    return 0;
+}
+
---
+++ notes.md
@@ -0,0 +1,6 @@
+
+ g++ -std=c++17 lecture2.cpp 
+./a.out 
+
+ g++ -std=c++17 lecture2.cpp -o lectureCode
+ ./lectureCode
